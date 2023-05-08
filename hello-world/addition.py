b4da2404d89f6251431c5681a92c5bb42411294e
Sukhbir Singh <sukhbir2506@outlook.com>
2023-05-08 12:26:33 +0530

    Learn Python

---
+++ hello-world/addition.py
@@ -0,0 +1,5 @@
+print("calculator program")
+first_number = input("first number: ")
+second_number = input("second number: ")
+# print(first_number + second_number) #error
+print(int(first_number) + int(second_number))
