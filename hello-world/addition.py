3a40e042f9c554832a6ed35b5607016b3cb5ddff
Sukhbir Singh <sukhbir2506@outlook.com>
2023-05-08 13:03:54 +0530

    .gitignore

--- hello-world/addition.py
+++
@@ -1,5 +0,0 @@
-print("calculator program")
-first_number = input("first number: ")
-second_number = input("second number: ")
-# print(first_number + second_number) #error
-print(int(first_number) + int(second_number))
