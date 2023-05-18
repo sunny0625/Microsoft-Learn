95657f2434141b168b111c91521ee12870a8afee
Sukhbir Singh <sukhbir2506@outlook.com>
2023-05-19 00:03:59 +0530

    Convert String to Number

---
+++ hello-world/string_to_number.py
@@ -0,0 +1,22 @@
+demo_int = int('215')
+print(demo_int)
+
+demo_float = float('215.3')
+print(demo_float)
+
+print(abs(39 - 16))
+print(abs(16 - 39))
+
+print(round(14.6))
+
+from math import ceil, floor
+
+round_up = ceil(12.5)
+print(round_up)
+
+round_down = floor(12.5)
+print(round_down)
+
+# Output
+# 13
+# 12
