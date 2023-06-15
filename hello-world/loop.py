6e0af0824531d125380bdb8b5a197ac0d1f8d09f
Sukhbir Singh <sukhbir2506@outlook.com>
2023-06-15 14:37:13 +0530

    Loops in Python

--- hello-world/loop.py
+++ hello-world/loop.py
@@ -17,3 +17,23 @@ while user_input.lower() != 'done':
         inputs.append(user_input)
     # Prompt for a new value
     user_input = input('Enter a new value, or done when done')
+
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+
+print("The first planet is ", planets[0])
+print("The second planet is ", planets[1])
+print("The third planet is ", planets[2])
+
+countdown = [4, 3, 2, 1, 0]
+for number in countdown:
+    print(number)
+print("Blast off!! 🚀")
+
+from time import sleep
+
+countdown = [4, 3, 2, 1, 0]
+
+for number in countdown:
+    print(number)
+    sleep(1)  # Wait 1 second
+print("Blast off!! 🚀")
