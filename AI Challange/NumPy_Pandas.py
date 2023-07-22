ab6611d404c8f8e6c656b100845e03c94fef6161
Sukhbir Singh <sukhbir2506@outlook.com>
2023-07-22 13:11:48 +0530

    AI Skill Challanges

---
+++ AI Challange/NumPy_Pandas.py
@@ -0,0 +1,129 @@
+# Exploring data arrays with NumPy
+
+data = [50,50,47,97,49,3,53,42,26,74,82,62,37,15,70,27,36,35,48,52,63,64]
+print(data)
+
+# 
+import numpy as np
+
+grades = np.array(data)
+print(grades)
+
+print (type(data),'x 2:', data * 2)
+print('---')
+print (type(grades),'x 2:', grades * 2)
+
+grades.shape
+
+grades[0]
+
+grades.mean()
+
+# Define an array of study hours
+study_hours = [10.0,11.5,9.0,16.0,9.25,1.0,11.5,9.0,8.5,14.5,15.5,
+               13.75,9.0,8.0,15.5,8.0,9.0,6.0,10.0,12.0,12.5,12.0]
+
+# Create a 2D array (an array of arrays)
+student_data = np.array([study_hours, grades])
+
+# display the array
+student_data
+
+# Show shape of 2D array
+student_data.shape
+
+# Show the first element of the first element
+student_data[0][0]
+
+# Get the mean value of each sub-array
+avg_study = student_data[0].mean()
+avg_grade = student_data[1].mean()
+
+print('Average study hours: {:.2f}\nAverage grade: {:.2f}'.format(avg_study, avg_grade))
+
+# Exploring tabular data with Pandas
+
+import pandas as pd
+
+df_students = pd.DataFrame({'Name': ['Dan', 'Joann', 'Pedro', 'Rosie', 'Ethan', 'Vicky', 'Frederic', 'Jimmie', 
+                                     'Rhonda', 'Giovanni', 'Francesca', 'Rajab', 'Naiyana', 'Kian', 'Jenny',
+                                     'Jakeem','Helena','Ismat','Anila','Skye','Daniel','Aisha'],
+                            'StudyHours':student_data[0],
+                            'Grade':student_data[1]})
+
+df_students 
+
+# Finding and filtering data in a DataFrame
+
+# Get the data for index value 5
+df_students.loc[5]
+
+# Get the rows with index values from 0 to 5
+df_students.loc[0:5]
+
+# Get data in the first five rows
+df_students.iloc[0:5]
+
+df_students.iloc[0,[1,2]]
+
+df_students.loc[0,'Grade']
+
+df_students.loc[df_students['Name']=='Aisha']
+
+df_students[df_students['Name']=='Aisha']
+
+df_students.query('Name=="Aisha"')
+
+df_students[df_students.Name == 'Aisha']
+
+# Loading a DataFrame from a file
+
+!wget https://raw.githubusercontent.com/MicrosoftDocs/mslearn-introduction-to-machine-learning/main/Data/ml-basics/grades.csv
+df_students = pd.read_csv('grades.csv',delimiter=',',header='infer')
+df_students.head()
+
+# Handling missing values
+
+df_students.isnull()
+
+df_students.isnull().sum()
+
+df_students[df_students.isnull().any(axis=1)]
+
+df_students.StudyHours = df_students.StudyHours.fillna(df_students.StudyHours.mean())
+df_students
+
+df_students = df_students.dropna(axis=0, how='any')
+df_students
+
+# Explore data in the DataFrame
+
+# Get the mean study hours using to column name as an index
+mean_study = df_students['StudyHours'].mean()
+
+# Get the mean grade using the column name as a property (just to make the point!)
+mean_grade = df_students.Grade.mean()
+
+# Print the mean study hours and mean grade
+print('Average weekly study hours: {:.2f}\nAverage grade: {:.2f}'.format(mean_study, mean_grade))
+
+# Get students who studied for the mean or more hours
+df_students[df_students.StudyHours > mean_study]
+
+# What was their mean grade?
+df_students[df_students.StudyHours > mean_study].Grade.mean()
+
+passes  = pd.Series(df_students['Grade'] >= 60)
+df_students = pd.concat([df_students, passes.rename("Pass")], axis=1)
+
+df_students
+
+print(df_students.groupby(df_students.Pass).Name.count())
+
+print(df_students.groupby(df_students.Pass)['StudyHours', 'Grade'].mean())
+
+# Create a DataFrame with the data sorted by Grade (descending)
+df_students = df_students.sort_values('Grade', ascending=False)
+
+# Show the DataFrame
+df_students
---
+++ AI Challange/Real_world_data.py
@@ -0,0 +1,238 @@
+# Exploring data with Python - real world data
+
+import pandas as pd
+from matplotlib import pyplot as plt
+
+# Load data from a text file
+!wget https://raw.githubusercontent.com/MicrosoftDocs/mslearn-introduction-to-machine-learning/main/Data/ml-basics/grades.csv
+df_students = pd.read_csv('grades.csv',delimiter=',',header='infer')
+
+# Remove any rows with missing data
+df_students = df_students.dropna(axis=0, how='any')
+
+# Calculate who passed, assuming '60' is the grade needed to pass
+passes  = pd.Series(df_students['Grade'] >= 60)
+
+# Save who passed to the Pandas dataframe
+df_students = pd.concat([df_students, passes.rename("Pass")], axis=1)
+
+
+# Print the result out into this notebook
+print(df_students)
+
+
+# Create a function that we can re-use
+def show_distribution(var_data):
+    '''
+    This function will make a distribution (graph) and display it
+    '''
+
+    # Get statistics
+    min_val = var_data.min()
+    max_val = var_data.max()
+    mean_val = var_data.mean()
+    med_val = var_data.median()
+    mod_val = var_data.mode()[0]
+
+    print('Minimum:{:.2f}\nMean:{:.2f}\nMedian:{:.2f}\nMode:{:.2f}\nMaximum:{:.2f}\n'.format(min_val,
+                                                                                            mean_val,
+                                                                                            med_val,
+                                                                                            mod_val,
+                                                                                            max_val))
+
+    # Create a figure for 2 subplots (2 rows, 1 column)
+    fig, ax = plt.subplots(2, 1, figsize = (10,4))
+
+    # Plot the histogram   
+    ax[0].hist(var_data)
+    ax[0].set_ylabel('Frequency')
+
+    # Add lines for the mean, median, and mode
+    ax[0].axvline(x=min_val, color = 'gray', linestyle='dashed', linewidth = 2)
+    ax[0].axvline(x=mean_val, color = 'cyan', linestyle='dashed', linewidth = 2)
+    ax[0].axvline(x=med_val, color = 'red', linestyle='dashed', linewidth = 2)
+    ax[0].axvline(x=mod_val, color = 'yellow', linestyle='dashed', linewidth = 2)
+    ax[0].axvline(x=max_val, color = 'gray', linestyle='dashed', linewidth = 2)
+
+    # Plot the boxplot   
+    ax[1].boxplot(var_data, vert=False)
+    ax[1].set_xlabel('Value')
+
+    # Add a title to the Figure
+    fig.suptitle('Data Distribution')
+
+    # Show the figure
+    fig.show()
+
+
+show_distribution(df_students['Grade'])
+
+
+# Get the variable to examine
+col = df_students['StudyHours']
+# Call the function
+show_distribution(col)
+
+# Get the variable to examine
+# We will only get students who have studied more than one hour
+col = df_students[df_students.StudyHours>1]['StudyHours']
+
+# Call the function
+show_distribution(col)
+
+# calculate the 0.01th percentile
+q01 = df_students.StudyHours.quantile(0.01)
+# Get the variable to examine
+col = df_students[df_students.StudyHours>q01]['StudyHours']
+# Call the function
+show_distribution(col)
+
+def show_density(var_data):
+    fig = plt.figure(figsize=(10,4))
+
+    # Plot density
+    var_data.plot.density()
+
+    # Add titles and labels
+    plt.title('Data Density')
+
+    # Show the mean, median, and mode
+    plt.axvline(x=var_data.mean(), color = 'cyan', linestyle='dashed', linewidth = 2)
+    plt.axvline(x=var_data.median(), color = 'red', linestyle='dashed', linewidth = 2)
+    plt.axvline(x=var_data.mode()[0], color = 'yellow', linestyle='dashed', linewidth = 2)
+
+    # Show the figure
+    plt.show()
+
+# Get the density of StudyHours
+show_density(col)
+
+for col_name in ['Grade','StudyHours']:
+    col = df_students[col_name]
+    rng = col.max() - col.min()
+    var = col.var()
+    std = col.std()
+    print('\n{}:\n - Range: {:.2f}\n - Variance: {:.2f}\n - Std.Dev: {:.2f}'.format(col_name, rng, var, std))
+
+    import scipy.stats as stats
+
+# Get the Grade column
+col = df_students['Grade']
+
+# get the density
+density = stats.gaussian_kde(col)
+
+# Plot the density
+col.plot.density()
+
+# Get the mean and standard deviation
+s = col.std()
+m = col.mean()
+
+# Annotate 1 stdev
+x1 = [m-s, m+s]
+y1 = density(x1)
+plt.plot(x1,y1, color='magenta')
+plt.annotate('1 std (68.26%)', (x1[1],y1[1]))
+
+# Annotate 2 stdevs
+x2 = [m-(s*2), m+(s*2)]
+y2 = density(x2)
+plt.plot(x2,y2, color='green')
+plt.annotate('2 std (95.45%)', (x2[1],y2[1]))
+
+# Annotate 3 stdevs
+x3 = [m-(s*3), m+(s*3)]
+y3 = density(x3)
+plt.plot(x3,y3, color='orange')
+plt.annotate('3 std (99.73%)', (x3[1],y3[1]))
+
+# Show the location of the mean
+plt.axvline(col.mean(), color='cyan', linestyle='dashed', linewidth=1)
+
+plt.axis('off')
+
+plt.show()
+
+df_students.describe()
+
+# Comparing data
+
+df_sample = df_students[df_students['StudyHours']>1]
+df_sample
+
+# Comparing numeric and categorical variables
+
+df_sample.boxplot(column='StudyHours', by='Pass', figsize=(8,5))
+
+# Comparing numeric variables
+
+# Create a bar plot of name vs grade and study hours
+df_sample.plot(x='Name', y=['Grade','StudyHours'], kind='bar', figsize=(8,5))
+
+from sklearn.preprocessing import MinMaxScaler
+
+# Get a scaler object
+scaler = MinMaxScaler()
+
+# Create a new dataframe for the scaled values
+df_normalized = df_sample[['Name', 'Grade', 'StudyHours']].copy()
+
+# Normalize the numeric columns
+df_normalized[['Grade','StudyHours']] = scaler.fit_transform(df_normalized[['Grade','StudyHours']])
+
+# Plot the normalized values
+df_normalized.plot(x='Name', y=['Grade','StudyHours'], kind='bar', figsize=(8,5))
+
+df_normalized.Grade.corr(df_normalized.StudyHours)
+
+# Create a scatter plot
+df_sample.plot.scatter(title='Study Time vs Grade', x='StudyHours', y='Grade')
+
+from scipy import stats
+
+#
+df_regression = df_sample[['Grade', 'StudyHours']].copy()
+
+# Get the regression slope and intercept
+m, b, r, p, se = stats.linregress(df_regression['StudyHours'], df_regression['Grade'])
+print('slope: {:.4f}\ny-intercept: {:.4f}'.format(m,b))
+print('so...\n f(x) = {:.4f}x + {:.4f}'.format(m,b))
+
+# Use the function (mx + b) to calculate f(x) for each x (StudyHours) value
+df_regression['fx'] = (m * df_regression['StudyHours']) + b
+
+# Calculate the error between f(x) and the actual y (Grade) value
+df_regression['error'] = df_regression['fx'] - df_regression['Grade']
+
+# Create a scatter plot of Grade vs StudyHours
+df_regression.plot.scatter(x='StudyHours', y='Grade')
+
+# Plot the regression line
+plt.plot(df_regression['StudyHours'],df_regression['fx'], color='cyan')
+
+# Display the plot
+plt.show()
+
+# Show the original x,y values, the f(x) value, and the error
+df_regression[['StudyHours', 'Grade', 'fx', 'error']]
+
+# Using the regression coefficients for prediction
+
+# Define a function based on our regression coefficients
+def f(x):
+    m = 6.3134
+    b = -17.9164
+    return m*x + b
+
+study_time = 14
+
+# Get f(x) for study time
+prediction = f(study_time)
+
+# Grade can't be less than 0 or more than 100
+expected_grade = max(0,min(100,prediction))
+
+#Print the estimated grade
+print ('Studying for {} hours per week may result in a grade of {:.0f}'.format(study_time, expected_grade))
+
